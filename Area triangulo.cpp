93d4ed736afbc20dfaf20a30f13e25016a4b8213
Roberto Montoya <roberto.montoya@alumnos.udg.mx>
2020-10-05 22:07:55 -0500

    Creacion del programa area triangulo

---
+++ Area triangulo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    unsigned int base;
+    unsigned int altura;
+
+    cin >> base;
+    cin >> altura;
+
+    cout << (base * altura / 2)<<endl;
+
+
+    return 0;
+}
